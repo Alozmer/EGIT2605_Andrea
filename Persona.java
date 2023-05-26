a5cca9ca9f156660810f07332185613605ecc02a
Andrea <a_andrea.lozano.mercado@iespablopicasso.es>
2023-05-26 14:27:51 +0200

    Clase Persona creada

---
+++ Persona.java
@@ -0,0 +1,41 @@
+public class Persona {
+	
+	    private String nombre;
+	    private String apellidos;
+	    private int edad;
+
+	    //Constructor
+	    public entradaPersona (){
+	        nombre = "";
+	        apellidos = "";
+	        edad = 0;
+	    }
+	    
+	    public void setnombre (String valornombre){
+	        nombre = "nombre" + valornombre; 
+	    }
+	    public void setapellidos (String valorapellidos){
+	        apellidos = "apellidos" + valorapellidos;
+	    }
+	    
+	    public void setedad (int valoredad){
+	        edad = valoredad;
+	    }
+	    
+	    public void cumpleanyos (int valoredad) {
+	    	edad = valoredad +1;
+	    }
+	   
+	    public String getnombre (){ 
+	    	return nombre; 
+	    }
+	  
+	    public String getapellidos () { 
+	    	return apellidos; 
+	    } 
+	    
+	    public int getedad () { 
+	    	return edad; 
+	    } 
+	}
+}
